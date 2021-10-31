1860b71ea5694b84dfb41e0b63f189963c33a360
Earle-Poole <eepoole4@gmail.com>
2021-10-31 14:09:58 -0500

    Merge udi's changes

--- src/organisms/Dashboard.tsx
+++ src/organisms/Dashboard.tsx
@@ -1,7 +1,15 @@
+import Container from '../molecules/container/Container'
+import Weather from '../molecules/weather/Weather'
 import styles from './dashboard.module.css'
 
 const Dashboard = () => {
-  return <div className={styles.dashboard}></div>
+  return (
+    <div className={styles.dashboard}>
+      <Container>
+        <Weather />
+      </Container>
+    </div>
+  )
 }
 
 export default Dashboard
