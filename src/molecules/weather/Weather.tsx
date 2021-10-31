de93651f47beb6071e5e00590480b1cfb5d43eec
Earle-Poole <eepoole4@gmail.com>
2021-10-31 14:48:18 -0500

    Added weather place holder

--- src/molecules/weather/Weather.tsx
+++ src/molecules/weather/Weather.tsx
@@ -1,7 +1,12 @@
 import styles from './weather.module.css'
+import weatherIcon from '../../assets/weather-placeholder.png'
 
 const Weather = () => {
-  return <div className={styles.weather}></div>
+  return (
+    <div className={styles.weather}>
+      <img src={weatherIcon} alt='weather icon' />
+    </div>
+  )
 }
 
 export default Weather
